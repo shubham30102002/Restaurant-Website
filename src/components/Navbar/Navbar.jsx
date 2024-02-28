ab1b5660137e413e9ee63d0ae77bd7f1036f8c9f
shubham30102002 <shubhamrg30@gmail.com>
2024-02-28 12:22:57 +0530

    navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -53,13 +53,13 @@ const Navbar = ({handleOrderPopup}) => {
             {
               NavLinks.map(({ id, name, link }) => (
                 <li key={id}>
-                  <a href={link} className="inline-block hover:text-primary text-xl font-semibold">{name} </a>
+                  <a href={link} className="hidden sm:inline-block hover:text-primary text-xl font-semibold">{name} </a>
                 </li>
               ))
             }
 
             {/* simple dropdown and links */}
-            <li className="cursor-pointer group ">
+            <li className="hidden md:block cursor-pointer group ">
               <a href="/#"
                 className="inline-block hover:text-primary text-xl font-semibold" >
                 <div className="flex items-center gap-[2px] py-2">
@@ -93,7 +93,7 @@ const Navbar = ({handleOrderPopup}) => {
             <li>
               <button 
               onClick={handleOrderPopup}
-              className="flex justify-center items-center gpa-2 bg-secondary text-xl h-[40px] text-white px-5 py-2 hover:scale-105 duration-300 ">
+              className="flex justify-center items-center gpa-2 bg-secondary text-xl h-[40px] text-white px-2 md:px-5 py-2 hover:scale-105 duration-300 ">
                 <FaUser />
                 My Account
               </button>
